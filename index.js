65b8ed079466fc88e26ce995f596a0246d3dfa13
blood8879 <blood8879@gmail.com>
2022-01-01 00:04:41 +0900

    20220101

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User.js");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -19,7 +21,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!!'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body);
 
     user.save((err, userInfo) => {
@@ -28,6 +30,59 @@ app.post('/register', (req, res) => {
             success: true
         })
     });
+})
+
+app.post('/api/users/login', (req, res) => {
+    // 요청된 이메일을 데이터베이스에서 찾음
+    User.findOne({ email: req.body.email }, (err, userInfo) => {
+        if(!userInfo) {
+            return res.json({
+                loginSuccess: false,
+                message: "일치하는 이메일이 없습니다."
+            })
+        }
+    })
+
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 일치하는지 확인
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if(!isMatch)
+            return res.json({ loginSuccess: false, message: "비밀번호가 다릅니다." })
+
+        // 비밀번호가 일치하면 토큰 생성
+        user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err);
+
+            // 토큰을 저장한다. where?? 쿠키
+            res.cookie("x_auth", user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id })
+        })
+    })
+})
+
+app.get('/api/users/auth', auth , (req, res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 것은 Authentication 이 true;
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: ""}
+        , (err, user) => {
+            if(err) return res.json({ success: false, err });
+            return res.status(200).send({ 
+                success: true
+            })
+        })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
