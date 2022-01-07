ef38c6dcabec8274f0f0b0770209679db000194c
blood8879 <blood8879@gmail.com>
2022-01-08 00:26:04 +0900

    20220108

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element = {<LandingPage/>} />
+        <Route exact path="/login" element = {<LoginPage/>} />
+        <Route exact path="/register" element = {<RegisterPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
